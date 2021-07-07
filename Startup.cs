f8390bd57fb5750623a0c71382c14e724859039d
aldoalfiyanv <aldoalfiyan72@gmail.com>
2021-07-07 19:12:10 +0700

    Penambahan Swagger UI

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
@@ -15,15 +16,12 @@ namespace Ocelot.Demo
 {
     public class Startup
     {
+        private IConfiguration configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleEF", Version = "v1" });
-            });
-
             var secret = "dRgUkXp2r5u8x/A?D(G+KbPeShVmYq3t";
             var key = Encoding.ASCII.GetBytes(secret);
             services.AddAuthentication(option =>
@@ -44,6 +42,7 @@ namespace Ocelot.Demo
             });
 
             services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+            services.AddSwaggerForOcelot(configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,11 +51,12 @@ namespace Ocelot.Demo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleEF v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
 
             app.UseRouting();
 
@@ -64,7 +64,10 @@ namespace Ocelot.Demo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
 
 
